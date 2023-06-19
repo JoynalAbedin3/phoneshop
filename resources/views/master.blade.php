b9647cb376aae93664f86668dce4e0ff5f8e3616
JoynalAbedin3 <engr.joynal.bd@gmail.com>
2023-06-20 00:04:21 +0600

    edit css js

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -13,15 +13,15 @@
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
     
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('/')}}/assets/frontend/css/bootstrap.min.css">
     
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{asset('/')}}/assets/frontend/css/font-awesome.min.css">
     
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/owl.carousel.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="{{asset('/')}}/assets/frontend/css/owl.carousel.css">
+    <link rel="stylesheet" href="{{asset('/')}}/assets/frontend/style.css">
+    <link rel="stylesheet" href="{{asset('/')}}/assets/frontend/css/responsive.css">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -562,17 +562,17 @@
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     
     <!-- jQuery sticky menu -->
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.sticky.js"></script>
+    <script src="{{asset('/')}}/assets/frontend/js/owl.carousel.min.js"></script>
+    <script src="{{asset('/')}}/assets/frontend/js/jquery.sticky.js"></script>
     
     <!-- jQuery easing -->
-    <script src="js/jquery.easing.1.3.min.js"></script>
+    <script src="{{asset('/')}}/assets/frontend/js/jquery.easing.1.3.min.js"></script>
     
     <!-- Main Script -->
-    <script src="js/main.js"></script>
+    <script src="{{asset('/')}}/assets/frontend/js/main.js"></script>
     
     <!-- Slider -->
-    <script type="text/javascript" src="js/bxslider.min.js"></script>
-	<script type="text/javascript" src="js/script.slider.js"></script>
+    <script type="text/javascript" src="{{asset('/')}}/assets/frontend/js/bxslider.min.js"></script>
+	<script type="text/javascript" src="{{asset('/')}}/assets/frontend/js/script.slider.js"></script>
   </body>
 </html>
